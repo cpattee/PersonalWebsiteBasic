26f744dd3ee7233e4902655a612d369626a6558a
Chelsea <cpattee@ucsd.edu>
2014-11-25 21:25:55 -0800

    thanks mooey! changes after initial commit

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/user');
 
+
+
 var app = express();
 
 // view engine setup
@@ -26,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,33 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Chelsea' });
 });
 
+
+/* GET projects page. */
+router.get('/projects', function(req, res) {
+  res.render('projects', { title: 'Projects' });
+});
+
+/* GET about page. */
+router.get('/about', function(req, res) {
+  res.render('about', { title: 'About' });
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res) {
+  res.render('contact', { title: 'Contact' });
+});
+
+/* GET test page. */
+router.get('/test', function(req, res) {
+  res.render('test', { title: 'Projects' });
+});
+
+/* GET cogs page. */
+router.get('/cogs187a', function(req, res) {
+  res.render('cogs187a', { title: 'Cogs' });
+});
 
 module.exports = router;
