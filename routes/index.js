745623830f042310c92b08a753ea358f6972c779
Chelsea <cpattee@ucsd.edu>
2014-12-01 09:11:51 -0800

    added project pages
    uploaded resized image files and started developing deeper level pages

--- routes/index.js
+++ routes/index.js
@@ -32,4 +32,29 @@ router.get('/cogs187a', function(req, res) {
   res.render('cogs187a', { title: 'Cogs' });
 });
 
+/* GET category page: "Design" */
+router.get('/projects/design', function(req, res) {
+  res.render('design', { title: 'Design' });
+});
+
+/* GET category page: "Design" */
+router.get('/projects/traditional', function(req, res) {
+  res.render('traditional', { title: 'Traditional' });
+});
+
+/* GET category page: "Programs" */
+router.get('/projects/programs', function(req, res) {
+  res.render('programs', { title: 'Programs' });
+});
+
+/* GET category page: "Data Visualization" */
+router.get('/projects/dataviz', function(req, res) {
+  res.render('dataviz', { title: 'Data Visualization' });
+});
+
+/* GET project page: "condom-pope". */
+router.get('/projects/design/condom-pope', function(req, res) {
+  res.render('condom-pope', { title: 'Condom Pope' });
+});
+
 module.exports = router;
